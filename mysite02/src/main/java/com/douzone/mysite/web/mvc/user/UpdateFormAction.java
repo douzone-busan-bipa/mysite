b79d101309dbcd41f1e079f91ac9988434dff4ef
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-26 15:00:32 +0900

    mysite - user #finish

--- mysite02/src/main/java/com/douzone/mysite/web/mvc/user/UpdateFormAction.java
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/user/UpdateFormAction.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.douzone.mysite.repository.UserRepository;
 import com.douzone.mysite.vo.UserVo;
 import com.douzone.web.mvc.Action;
 import com.douzone.web.util.WebUtil;
