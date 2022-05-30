42df09e46e46196640b604ba7a9cd78302a370b1
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-05-30 11:21:07 +0900

    listener & filter

--- mysite02/src/main/java/com/douzone/mysite/controller/GuestbookController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -13,7 +13,8 @@ public class GuestbookController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+		// request.setCharacterEncoding("UTF-8");
+		// Encoding Filter 에서 처리
 		
 		String actionName = request.getParameter("a");
 
--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -14,7 +14,9 @@ public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
+		// request.setCharacterEncoding("UTF-8");
+		// Encoding Filter 에서 처리
+		
 		String actionName = request.getParameter("a");
 		
 		ActionFactory factory = new UserActionFactory();
