fa98049fca6e47c795c5e0914ef1cbd874bab27c
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-07 18:23:28 +0900

    mysite - guestbook #finish

--- mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/GuestbookService.java
@@ -2,21 +2,30 @@ package com.douzone.mysite.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.douzone.mysite.repository.GuestbookRepository;
 import com.douzone.mysite.vo.GuestbookVo;
 
 @Service
 public class GuestbookService {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+	
 	public List<GuestbookVo> getMessageList() {
-		return null;
+		return guestbookRepository.findAll();
 	}
 	
 	public Boolean deleteMessage(Long no, String password) {
-		return false;
+		GuestbookVo vo = new GuestbookVo();
+		vo.setNo(no);
+		vo.setPassword(password);
+		
+		return guestbookRepository.delete(vo);
 	}
 	
 	public Boolean addMessage(GuestbookVo vo) {
-		return false;
+		return guestbookRepository.insert(vo);
 	}
 }
