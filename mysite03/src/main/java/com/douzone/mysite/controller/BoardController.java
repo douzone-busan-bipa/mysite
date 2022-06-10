bf38e3be0cfc44728643483acd2ce2d0baf5d1c6
더존비즈온 <103369445+douzone-busan-bipa@users.noreply.github.com>
2022-06-10 15:44:05 +0900

    Add files via upload

--- mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -1,21 +1,22 @@
-package com.poscoict.mysite.controller;
+package com.douzone.mysite.controller;
 
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.poscoict.mysite.security.Auth;
-import com.poscoict.mysite.security.AuthUser;
-import com.poscoict.mysite.service.BoardService;
-import com.poscoict.mysite.vo.BoardVo;
-import com.poscoict.mysite.vo.UserVo;
-import com.poscoict.web.util.WebUtil;
+import com.douzone.mysite.security.Auth;
+import com.douzone.mysite.security.AuthUser;
+import com.douzone.mysite.service.BoardService;
+import com.douzone.mysite.vo.BoardVo;
+import com.douzone.mysite.vo.UserVo;
+import com.douzone.web.util.WebUtil;
 
 @Controller
 @RequestMapping("/board")
@@ -26,10 +27,8 @@ public class BoardController {
 	@RequestMapping("")
 	public String index(@RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword, Model model) {
 		Map<String, Object> map = boardService.getContentsList(page, keyword);
-		
 		model.addAttribute("map", map);
-		// 참고: model.addAllAttributes(map);
-		
+		// model.addAllAttributes(map);
 		return "board/index";
 	}
 
@@ -40,26 +39,26 @@ public class BoardController {
 		return "board/view";
 	}
 
-	@Auth
+	@Auth(role = "USER")
 	@RequestMapping("/delete/{no}")
-	public String delete(UserVo authUser, @PathVariable("no") Long boardNo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
+	public String delete(@AuthUser UserVo authUser, @PathVariable("no") Long boardNo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
 		boardService.deleteContents(boardNo, authUser.getNo());
 		return "redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 
 	@Auth
-	@RequestMapping(value = "/update/{no}")
-	public String update(@AuthUser UserVo authUser, @PathVariable("no") Long no, Model model) {
+	@RequestMapping(value = "/modify/{no}")
+	public String modify(@AuthUser UserVo authUser, @PathVariable("no") Long no, Model model) {
 		BoardVo boardVo = boardService.getContents(no, authUser.getNo());
 		model.addAttribute("boardVo", boardVo);
-		return "board/update";
+		return "board/modify";
 	}
 
 	@Auth
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public String update(@AuthUser UserVo authUser, BoardVo boardVo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String modify(@AuthUser UserVo authUser, @ModelAttribute BoardVo boardVo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
 		boardVo.setUserNo(authUser.getNo());
-		boardService.updateContents(boardVo);
+		boardService.modifyContents(boardVo);
 		return "redirect:/board/view/" + boardVo.getNo() + "?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 
@@ -71,7 +70,7 @@ public class BoardController {
 
 	@Auth
 	@RequestMapping(value = "/write", method = RequestMethod.POST)
-	public String write(@AuthUser UserVo authUser, BoardVo boardVo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
+	public String write(@AuthUser UserVo authUser, @ModelAttribute BoardVo boardVo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
 		boardVo.setUserNo(authUser.getNo());
 		boardService.addContents(boardVo);
 		return "redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
@@ -79,7 +78,7 @@ public class BoardController {
 
 	@Auth
 	@RequestMapping(value = "/reply/{no}")
-	public String reply(@AuthUser UserVo authUser, @PathVariable("no") Long no, Model model) {
+	public String reply(@PathVariable("no") Long no, Model model) {
 		BoardVo boardVo = boardService.getContents(no);
 		boardVo.setOrderNo(boardVo.getOrderNo() + 1);
 		boardVo.setDepth(boardVo.getDepth() + 1);
--- mysite03/src/main/java/com/douzone/mysite/controller/GalleryController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GalleryController.java
@@ -1,4 +1,6 @@
-package com.poscoict.mysite.controller;
+package com.douzone.mysite.controller;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,38 +11,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.poscoict.mysite.service.FileUploadService;
+import com.douzone.mysite.security.Auth;
+import com.douzone.mysite.service.FileUploadService;
+import com.douzone.mysite.service.GalleryService;
+import com.douzone.mysite.vo.GalleryVo;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
-	@Autowired
-	private FileUploadService fileUploadService;
 	
-	// @Autowired
-	// private GalleryService galleryService;
+	@Autowired
+	private GalleryService galleryService;
+	
+	@Autowired
+	private FileUploadService FileUploadService;
 	
 	@RequestMapping("")
 	public String index(Model model) {
-		// List<GalleryVo> list = galleryService.getImages();
-		// model.addAttribute("list", list);
+		List<GalleryVo> list = galleryService.getImages();
+		model.addAttribute("list", list);
 		return "gallery/index";
 	}
 	
-	// @Auth(role="ADMIN")
-	@RequestMapping("/delete/{no}")
-	public String delete(@PathVariable("no") Long no) {
-		 // galleryService.removeImge(no);
-		System.out.println("delete:" + no);
+	@Auth(role="ADMIN")
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+	public String upload(@RequestParam("file") MultipartFile file, @RequestParam(value="comments", required=true, defaultValue="") String comments) {
+		String url = FileUploadService.restoreImage(file);
+		
+		GalleryVo vo = new GalleryVo();
+		vo.setUrl(url);
+		vo.setComments(comments);
+		galleryService.saveImage(vo);
+		
 		return "redirect:/gallery";
 	}
 	
-	@RequestMapping(value="/upload", method=RequestMethod.POST)
-	public String upload(
-			@RequestParam("file") MultipartFile multipartFile,
-			@RequestParam(value="comments", required=true, defaultValue="") String comments) {
-		// galleryService.saveImage(vo);
-		System.out.println("comments:" + comments);
+	@Auth(role="ADMIN")
+	@RequestMapping(value="/delete/{no}")
+	public String delete(@PathVariable("no") Long no) {
+		galleryService.removeImage(no);
 		return "redirect:/gallery";
 	}
+	
 }
