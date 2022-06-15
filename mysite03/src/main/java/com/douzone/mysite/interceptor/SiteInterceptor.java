c5c6d7ea599c4b5f23a2ed9a518fe483b8bf7897
더존비즈온 <103369445+douzone-busan-bipa@users.noreply.github.com>
2022-06-15 17:22:06 +0900

    Update SiteInterceptor.java

--- mysite03/src/main/java/com/douzone/mysite/interceptor/SiteInterceptor.java
+++ mysite03/src/main/java/com/douzone/mysite/interceptor/SiteInterceptor.java
@@ -1,5 +1,29 @@
-package com.douzone.mysite.interceptor;
+package com.dozuone.mysite.interceptor;
 
-public class SiteInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.dozuone.mysite.service.SiteService;
+import com.dozuone.mysite.vo.SiteVo;
+
+public class SiteInterceptor extends HandlerInterceptorAdapter {
+	
+	@Autowired
+	private SiteService siteService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		SiteVo site = (SiteVo)request.getServletContext().getAttribute("site");
+		if(site == null) {
+			site = siteService.getSite();
+			request.getServletContext().setAttribute("site", site);
+		}
+		
+		return true;
+	}
 
 }
